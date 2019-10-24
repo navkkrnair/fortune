6392093e9c841b00b17d496888970f366043de6a
navkk <navkkrnair@gmail.com>
2019-10-24 16:51:43 +0530

    Changed code for environment variables

--- fortuneloop.sh
+++ fortuneloop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 trap "exit" SIGINT
-INTERVAL=$1
+echo Configured to generate new fortune every $INTERVAL seconds
+mkdir -p /var/htdocs
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
