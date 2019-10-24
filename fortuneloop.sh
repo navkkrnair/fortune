ffd3efd2e79de99ba9a7659cd0ff286fc956f435
navkk <navkkrnair@gmail.com>
2019-10-24 13:54:46 +0530

    Changed code to test Commandline argument from Dockerfile

--- fortuneloop.sh
+++ fortuneloop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 trap "exit" SIGINT
+INTERVAL=$1
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
   /usr/games/fortune > /var/htdocs/index.html
-  sleep 10
+  sleep $INTERVAL
 done
